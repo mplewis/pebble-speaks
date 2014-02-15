1566d30d3aa907799b508ac454ee8472b5e24755
Jiexi Cao <jiexicao@gmail.com>
2014-02-15 17:28:11 -0500

    web form now sends data to server

--- webform/home.js
+++ webform/home.js
@@ -2,7 +2,35 @@ $(function() {
     $("#submit").on("click", function() {
         var speechName = $("#speechName").val();
         var topicPhrases = document.getElementById("topicPhrases").value;
-        console.log(speechName);
-        console.log(topicPhrases);
+
+        topicPhrases = topicPhrases.split('\n');
+        // Trim trailing whitespace
+        for (var i in topicPhrases) {
+            topicPhrases[i] = topicPhrases[i].trim();
+        }
+
+        var speechData = {
+            speechName: speechName,
+            topicPhrases: topicPhrases,
+        };
+
+        var jsonSpeech = JSON.stringify(speechData);
+        console.log(jsonSpeech);
+
+        var server = "http://107.170.13.33:3000/speeches";
+
+        $.ajax({
+            type: "POST",
+            url: reqbin,
+            data: JSON.stringify(speechData),
+            contentType: "application/json",
+            dataType: "json",
+            success: function(data){
+                console.log(data)
+            },
+            failure: function(err){
+                console.log(err)
+            }
+        });
     });
 });
