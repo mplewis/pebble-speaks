46d67d1c0ed14ea2f43959a6d2984a1303059846
He Li <heli@college.harvard.edu>
2014-02-15 06:54:10 -0500

    added home menu

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var SECS_BEFORE_SPEECH = 5;
 var speeches;
 var currSpeech = 0;
 var currentMode = 'loading'; // 'speechSelect', 'speechRun'
+var practice_or_do = 'practice';
+var options;
+var currOption = 0;
 
 simply.setText({title: 'Loading...'}, true);
 
@@ -64,6 +67,18 @@ function loadSpeeches(url, callback) {
   callback(allSpeeches);
 }
 
+function loadHomeScreen(url, callback) {
+  var homeOptions = [
+    { title: 'Do Speech' },
+    { title: 'Practice' },
+  ];
+  callback(homeOptions);
+}
+
+function displayHomeOption(currentOption) {
+  simply.setText({title: currentOption}, true);
+}
+
 function selectSpeech(speech) {
   currentMode = 'speechRun';
   simply.title(speech.title);
@@ -79,11 +94,31 @@ function selectSpeech(speech) {
     simply.subtitle('Starts in ' + countdown + '...');
   }, 1000);
   simply.subtitle('Starts in ' + countdown + '...');
+  currentMode = 'home'
+  return;
+
 }
 
 simply.on('singleClick', function(event) {
-  if (currentMode === 'speechSelect') {
-
+  if (currentMode === 'home') {
+    if (event.button === 'up') {
+      if (currOption > 0) {
+        currOption--;
+        displayHomeOption(options[currOption].title);
+      }
+    } else if (event.button === 'down') {
+      if (currOption + 1 < options.length) {
+        currOption++;
+        displayHomeOption(options[currOption].title);
+      }
+    } else if (event.button === 'select') {
+        loadSpeeches('http://www.whatever.com/', function(speechesRetrieved) {
+          currentMode = 'speechSelect';
+          speeches = speechesRetrieved;
+          displaySpeech(speeches[0]);
+        })
+    }
+  } else if (currentMode === 'speechSelect') {
     if (event.button === 'up') {
       if (currSpeech > 0) {
         currSpeech--;
@@ -97,12 +132,27 @@ simply.on('singleClick', function(event) {
     } else if (event.button === 'select') {
       selectSpeech(speeches[currSpeech]);
     }
-    
+
   }
 });
 
+
+loadHomeScreen('some bullshit url', function(optionsRetrieved) {
+  currentMode = 'home';
+  options = optionsRetrieved;
+  displayHomeOption(options[0].title);
+});
+
+/*
 loadSpeeches('http://www.whatever.com/', function(speechesRetrieved) {
   currentMode = 'speechSelect';
   speeches = speechesRetrieved;
   displaySpeech(speeches[0]);
 });
+*/
+
+/*ajax({ url: 'ENTER URL HERE' }, function(speechesRetrieved){
+  currentMode = 'speechSelect';
+  var speeches = speechesRetrieved;
+  displaySpeech(speeches[0]);
+});*/
