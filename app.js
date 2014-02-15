1faef6435b29016f5f46be941fc70d8ac55eb0ca
Jiexi Cao <jiexicao@gmail.com>
2014-02-15 08:04:43 -0500

    Fixed styling/structur, but functionality remains the same.

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 var SECS_BEFORE_SPEECH = 5;
 
-var speeches;
-var currSpeech = 0;
-var currentMode = 'loading'; // 'speechSelect', 'speechRun'
-var practice_or_do = 'practice';
-var options;
-var currOption = 0;
+// Namespaces
+var app = {};
+var data = {};
+data.homeOptions = [];
+data.allModes = [
+  { title: 'Do Speech' },
+  { title: 'Practice' },
+];
+
+data.allSpeeches = [
+  {
+    title: 'Pet animals',
+    sections: [
+      {seconds: 60, topic: 'Kittens'},
+      {seconds: 20, topic: 'Puppies'},
+      {seconds: 30, topic: 'Birdies'}
+    ]
+  },
+  {
+    title: 'Deadly animals',
+    sections: [
+      {seconds: 29, topic: 'Snakes'},
+      {seconds: 255, topic: 'Sharks'}
+    ]
+  },
+  {
+    title: 'Awesome foods',
+    sections: [
+      {seconds: 100, topic: 'Chaat'},
+      {seconds: 601, topic: 'Pizza'},
+      {seconds: 331, topic: 'Salmon'},
+      {seconds: 33, topic: 'Filet mignon'}
+    ]
+  },
+];
+
+app.currentScreen = 'loading'; // 'speechSelect', 'speechRun'
+app.currentMode = 'practice'; // 'doSpeech'
+app.currentSpeech = '';
 
 simply.setText({title: 'Loading...'}, true);
 
-function secsToMMSS(secs) {
+app.secsToMMSS = function(secs) {
   var minutes = 0;
   while (secs >= 60) {
     secs -= 60;
@@ -20,68 +53,65 @@ function secsToMMSS(secs) {
   if (secs < 10)
     secs = '0' + secs;
   return minutes + ':' + secs;
-}
+};
 
-function displaySection(sectionData) {
-  var mmss = secsToMMSS(sectionData.seconds);
-  simply.setText({title: mmss}, true);
-  simply.subtitle(sectionData.topic);
-}
-
-function displaySpeech(speechData) {
+app.displaySpeech = function(speechData) {
   simply.setText({title: speechData.title}, true);
   simply.subtitle(speechData.sections.length + ' sections');
-}
+};
 
-function sectionData(speechData, sectionNum) {
-  return speechData.sections[sectionNum];
-}
+app.displayHomeOption = function() {
+  var currIndex = 0;
+  simply.setText({title: data.allModes[currIndex].title}, true);
 
-function loadSpeeches(url, callback) {
-  var allSpeeches = [
-    {
-      title: 'Pet animals',
-      sections: [
-        {seconds: 60, topic: 'Kittens'},
-        {seconds: 20, topic: 'Puppies'},
-        {seconds: 30, topic: 'Birdies'}
-      ]
-    },
-    {
-      title: 'Deadly animals',
-      sections: [
-        {seconds: 29, topic: 'Snakes'},
-        {seconds: 255, topic: 'Sharks'}
-      ]
-    },
-    {
-      title: 'Awesome foods',
-      sections: [
-        {seconds: 100, topic: 'Chaat'},
-        {seconds: 601, topic: 'Pizza'},
-        {seconds: 331, topic: 'Salmon'},
-        {seconds: 33, topic: 'Filet mignon'}
-      ]
-    },
-  ];
-  callback(allSpeeches);
-}
+  simply.on('singleClick', function(event) {
+    if (event.button === 'up') {
+      if (currIndex > 0) {
+        currIndex--;
+        simply.setText({title: data.allModes[currIndex].title}, true);
+      }
+    } else if (event.button === 'down') {
+      if (currIndex + 1 < data.allModes.length) {
+        currIndex++;
+        simply.setText({title: data.allModes[currIndex].title}, true);
+      }
+    } else if (event.button === 'select') {
+      app.currentScreen = 'speechSelect';
+      app.currentMode = data.allModes[currIndex].title;
+      app.selectSpeech();
+    }
+  });
+};
 
-function loadHomeScreen(url, callback) {
-  var homeOptions = [
-    { title: 'Do Speech' },
-    { title: 'Practice' },
-  ];
-  callback(homeOptions);
-}
+app.selectSpeech = function() {
+  var displaySpeech = function(speechData) {
+    simply.setText({title: speechData.title}, true);
+    simply.subtitle(speechData.sections.length + ' sections');
+  };
 
-function displayHomeOption(currentOption) {
-  simply.setText({title: currentOption}, true);
-}
+  var currIndex = 0;
+  displaySpeech(data.allSpeeches[currIndex]);
 
-function selectSpeech(speech) {
-  currentMode = 'speechRun';
-  simply.title(speech.title);
+  simply.on('singleClick', function(event) {
+    if (event.button === 'up') {
+      if (currIndex > 0) {
+        currIndex--;
+        displaySpeech(data.allSpeeches[currIndex]);
+      }
+    } else if (event.button === 'down') {
+      if (currIndex + 1 < data.allSpeeches.length) {
+        currIndex++;
+        displaySpeech(data.allSpeeches[currIndex]);
+      }
+    } else if (event.button === 'select') {
+      app.currentSpeech = data.allSpeeches[currIndex];
+      app.currentScreen = 'speechRun';
+      app.runSpeech();
+    }
+  });
+};
+
+app.runSpeech = function() {
   var countdown = SECS_BEFORE_SPEECH;
   var ctdToSpeechStart = setInterval(function() {
     countdown--;
@@ -94,66 +124,9 @@ function selectSpeech(speech) {
     }
     simply.subtitle('Starts in ' + countdown + '...');
   }, 1000);
-  simply.subtitle('Starts in ' + countdown + '...');
-  currentMode = 'home'
-  return;
 
-}
-
-simply.on('singleClick', function(event) {
-  if (currentMode === 'home') {
-    if (event.button === 'up') {
-      if (currOption > 0) {
-        currOption--;
-        displayHomeOption(options[currOption].title);
-      }
-    } else if (event.button === 'down') {
-      if (currOption + 1 < options.length) {
-        currOption++;
-        displayHomeOption(options[currOption].title);
-      }
-    } else if (event.button === 'select') {
-        loadSpeeches('http://www.whatever.com/', function(speechesRetrieved) {
-          currentMode = 'speechSelect';
-          speeches = speechesRetrieved;
-          displaySpeech(speeches[0]);
-        })
-    }
-  } else if (currentMode === 'speechSelect') {
-    if (event.button === 'up') {
-      if (currSpeech > 0) {
-        currSpeech--;
-        displaySpeech(speeches[currSpeech]);
-      }
-    } else if (event.button === 'down') {
-      if (currSpeech + 1 < speeches.length) {
-        currSpeech++;
-        displaySpeech(speeches[currSpeech]);
-      }
-    } else if (event.button === 'select') {
-      selectSpeech(speeches[currSpeech]);
-    }
-
-  }
-});
-
-
-loadHomeScreen('some bullshit url', function(optionsRetrieved) {
   currentMode = 'home';
-  options = optionsRetrieved;
-  displayHomeOption(options[0].title);
-});
+  return;
+};
 
-/*
-loadSpeeches('http://www.whatever.com/', function(speechesRetrieved) {
-  currentMode = 'speechSelect';
-  speeches = speechesRetrieved;
-  displaySpeech(speeches[0]);
-});
-*/
-
-/*ajax({ url: 'ENTER URL HERE' }, function(speechesRetrieved){
-  currentMode = 'speechSelect';
-  var speeches = speechesRetrieved;
-  displaySpeech(speeches[0]);
-});*/
+app.displayHomeOption();
